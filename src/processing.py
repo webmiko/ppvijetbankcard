c94e842791d44d6a0f9f37b8a5b157a8c961290c
webmiko <webmiko@icloud.com>
2025-10-14 12:07:42 +0300

    изменение названия функции с булевым значением с reverse_data на is_reverse

--- src/processing.py
+++ src/processing.py
@@ -22,16 +22,16 @@ def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
     return [item for item in list_of_dicts if item.get("state") == state]
 
 
-def sort_by_date(list_of_dicts: list, reverse_order: bool = True) -> list:
+def sort_by_date(list_of_dicts: list, is_reverse: bool = True) -> list:
     """
     Сортирует список словарей по ключу 'date' в формате ISO.
 
     Args:
         list_of_dicts: Список словарей, содержащих ключи 'date'
-        reverse_order: Порядок сортировки (по умолчанию True - по убыванию)
+        is_reverse: Порядок сортировки (по умолчанию True - по убыванию)
 
     Returns:
         Новый список, отсортированный по дате
     """
     # Сортировка с использованием преобразования строки даты в объект datetime
-    return sorted(list_of_dicts, key=lambda x: datetime.fromisoformat(x["date"]), reverse=reverse_order)
+    return sorted(list_of_dicts, key=lambda x: datetime.fromisoformat(x["date"]), reverse=is_reverse)
